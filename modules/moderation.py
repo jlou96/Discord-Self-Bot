99954219bcfe57c36cccb3060ac4a0d5a9d4671d
DiNitride <jamesthecookie@gmail.com>
2016-11-22 21:51:56 +0000

    Revert "Overhaul"
    This reverts commit 9de3b2dec321d97ef2d205f829aaa50a6a27737a.

--- modules/moderation.py
+++ modules/moderation.py
@@ -5,15 +5,27 @@ class Moderation():
     def __init__(self, bot):
         self.bot = bot
 
-    # Banning and Kicking commands
+    ##############################
+    ## Misc moderation commands ##
+    ##############################
+
+    # Changes the bot's game
+    @commands.command(pass_conext=True)
+    async def changegame(self, *, game: str):
+        """Updates the Bot's game"""
+        # Update the bots game
+        await self.bot.change_status(discord.Game(name=game))
+        await self.bot.say("Game updated.")
+        print("Updated Bot's Game")
+
+    ##################################
+    ## Banning and Kicking commands ##
+    ##################################
 
     # Bans a member
     @commands.command()
     async def ban(self, member: discord.Member = None):
-        """Bans a member
-        Usage:
-        $ban @recchan
-        User must have ban member permissions"""
+        """Bans a member"""
         # Are they trying to ban nobody? Are they stupid?
         # Why do they have mod powers if they're this much of an idiot?
         if member is None:
@@ -25,35 +37,34 @@ class Moderation():
         # Bans the user
         await self.bot.ban(member, delete_message_days=1)
         # Prints to console
+        print("{0.name} has been banned".format(member))
 
     # Kicks a member
     @commands.command()
     async def kick(self, member: discord.Member = None):
-        """Kicks a member
-        Usage:
-        $kick @recchan
-        User must have kick member permissions"""
-        # Same as above, are they stupid
+        """Kicks a member"""
+        # Are they trying to kick nobody? Are they stupid?
+        # Why do they have mod powers if they're this much of an idiot?
         if member is None:
             return
-        # Still not allowed to kick me
+        # Is the person being banned me? No we don't allow that
         elif member.id == '95953002774413312':
             await self.bot.say("http://i.imgur.com/BSbBniw.png")
             return
-        # Kicks the user
+        # Bans the user
         await self.bot.kick(member)
         # Prints to console
+        print("{0.name} has been kicked".format(member))
 
-    # Information commands
-    # Server info and member info
+    #################################
+    ## Information commands        ##
+    ## Server info and member info ##
+    #################################
 
     # Gives the user some basic info on a user
     @commands.command(pass_context=True)
     async def info(self, ctx, member : discord.Member = None):
-        """Infomation on a user
-        Usage:
-        $info @DiNitride
-        If no member is specified, it defaults to the sender"""
+        """Gives basic info and shows profile image of a user."""
         if member == None:
             member = ctx.message.author
         await self.bot.say(
@@ -65,13 +76,12 @@ class Moderation():
             "Bot?: {0.bot}\n".format(member) +
             "```" +
             "\n{0.avatar_url}".format(member))
+        print("Run: info on {0.name}".format(member))
 
     # Server Info
     @commands.command(pass_context=True)
     async def serverinfo(self, ctx):
-        """Shows server information
-        Usage:
-        $serverinfo"""
+        """Basic info on the server."""
         server = ctx.message.server
         afk = server.afk_timeout / 60
         await self.bot.say(
@@ -84,6 +94,7 @@ class Moderation():
             "AFK Timeout and Channel: {0} minutes in '{1.afk_channel}'\n".format(afk, server) +
             "Member Count: {0.member_count}\n".format(server) +
             "```")
+        print("Run: Server Info")
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
--- self_bot.py
+++ self_bot.py
@@ -2,20 +2,14 @@ import discord
 from discord.ext import commands
 import json
 import asyncio
-import inspect
 
 # Set's bot's desciption and prefixes in a list
 description = "A self bot to do things that are useful"
-bot = commands.Bot(command_prefix=['self.', "s."], description=description, self_bot=True)
+bot = commands.Bot(command_prefix=['self.'], description=description, self_bot=True)
 
-
-ready_modules = {
-    "moderation": "Moderation"
-}
-
-live_modules = {}
-
-########################################################################################################################
+###################
+## Startup Stuff ##
+###################
 
 @bot.event
 async def on_ready():
@@ -26,112 +20,47 @@ async def on_ready():
     print(bot.user.id)
     print("---------------------------")
 
-    with open("config/load-config.json") as file:
-        bot.load_config = json.load(file)
-
-    with open("config/config.json") as file:
-        bot.config = json.load(file)
-
     # Outputs the state of loading the modules to the console
     # So I know they have loaded correctly
     print("Loading Modules")
     print("---------------------------")
-
-    if bot.load_config["moderation"] == True:
-        bot.load_extension("modules.moderation")
-        live_modules["moderation"] = "Moderation"
-        print("loaded Moderation")
-
+    bot.load_extension("modules.misc")
+    print("Loaded Misc")
+    bot.load_extension("modules.moderation")
+    print("Loaded Moderation")
+    bot.load_extension("modules.rng")
+    print("loaded RNG")
     print("---------------------------")
 
-    await bot.change_presence(afk=True, status=discord.Status.idle)
-
-    asyncio.Task(save_configs())
-
-########################################################################################################################
+######################
+## Misc and Testing ##
+######################
 
 # Ping Pong
 # Testing the response of the bot
-@bot.command()
-async def ping():
+@bot.command(pass_context=True,hidden=True)
+async def ping(ctx):
     """Pong"""
     await bot.say("Pong")
+    print("Ping Pong")
 
+# Invite link to the bot server
+@bot.command()
+async def server():
+    """The bot's server, for updates or something"""
+    await bot.say("https://discord.gg/Eau7uhf")
+    print("Run: Server")
+
+# Bot's source code
 @bot.command()
 async def source():
     """Source code"""
-    await bot.say("https://github.com/DiNitride/Discord-Self-Bot")
-
-@bot.command(pass_context=True, name="eval")
-async def eval_(ctx, *, code: str):
-    """Evaluates a line of code provided"""
-    code = code.strip("` ")
-    server = ctx.message.server
-    message = ctx.message
-    try:
-        result = eval(code)
-        if inspect.isawaitable(result):
-            result = await result
-    except Exception as e:
-        await bot.say("```py\nInput: {}\n{}: {}```".format(code, type(e).__name__, e))
-    else:
-        await bot.say("```py\nInput: {}\nOutput: {}\n```".format(code, result))
-    await bot.delete_message(message)
-
-########################################################################################################################
-
-## Module loading and unloading
-@bot.command()
-async def load(extension_name: str):
-    """Loads an extension."""
-    extension_name = extension_name.lower()
-    load_extension_name = "modules." + extension_name
-
-    try:
-        bot.load_extension(load_extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-
-    live_modules.append(extension_name)
-    bot.load_config[extension_name] = True
-    await bot.say("{} loaded.".format(extension_name))
+    await bot.say("https://github.com/DiNitride/GAFBot")
+    print("Run: Source")
 
 @bot.command()
-async def unload(extension_name: str):
-    """Unloads an extension."""
-    extension_name = extension_name.lower()
-    unload_extension_name = "modules." + extension_name
-
-    if extension_name in live_modules:
-
-        bot.unload_extension(unload_extension_name)
-        bot.load_config[extension_name] = False
-        await bot.say("{} unloaded.".format(extension_name))
-
-    else:
-        await bot.say("Could not unload module, module not loaded.\nTo see loaded modules, do s.loaded")
-
-@bot.command()
-async def loaded():
-    output = "```Loaded Modules:"
-    for x in live_modules:
-        output += "\n- {}".format(ready_modules[x])
-    output += "```"
-    await bot.say(output)
-
-# Config save loop
-async def save_configs():
-    while True:
-        save = json.dumps(bot.config)
-        with open("config/config.json", "w") as file:
-            file.write(save)
-        save = json.dumps(bot.load_config)
-        with open("config/load-config.json", "w") as file:
-            file.write(save)
-        await asyncio.sleep(5)
-
-########################################################################################################################
+async def gaf_server():
+    """GAF Server Invite Link"""
 
 ##############################
 ## FANCY TOKEN LOGIN STUFFS ##
